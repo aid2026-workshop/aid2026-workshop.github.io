31f37f5b476cc17393b18733800cb0d6c9a3554f
trifle19 <33209154+trifle19@users.noreply.github.com>
2026-08-13 06:23:48 +0000

    Deploying to gh-pages from @ aid2026-workshop/aid2026-workshop.github.io@0abb362d1298e4a3ad48cce6750b8dd64e4f07ee 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,59 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-home",
+    title: "Home",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-accepted-papers",
+          title: "Accepted papers",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/papers/";
+          },
+        },{id: "nav-keynote-speech",
+          title: "Keynote speech",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/keynote/";
+          },
+        },{id: "nav-schedule",
+          title: "Schedule",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/schedule/";
+          },
+        },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
